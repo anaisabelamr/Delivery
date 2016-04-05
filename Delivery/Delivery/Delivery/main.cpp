74944e633f1772e780c99f07df7dee95604b5146
FACOMP <FACOMP@LAB1-PC24>
2016-04-05 13:41:53 -0300

    Menu vector feito

--- Delivery/Delivery/Delivery/main.cpp
+++ Delivery/Delivery/Delivery/main.cpp
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <vector>
+#include <iostream>
+#include <string.h>
 
-int main(int argc, char **argv)
+#include "Servico.h"
+#include "Delivery.h"
+#include "Pizzaria.h"
+#include "Sushi.h"
+#include "Buffet.h"
+#include "Pessoa.h"
+#include "Cliente.h"
+#include "Funcionario.h"
+
+using namespace std;
+
+int main()
 {
-	printf("hello world\n");
+
+    vector < Delivery *> delivery;
+    
+    int menuOpcao;
+    
+    cout << "   DELIVERY MANIA          \n       MENU       \n\n[1] Pizzaria\n[2] Sushi Bar\n[3] Servico de Buffet\nEscolha: ";
+    cin >> menuOpcao;
+    
 	return 0;
 }
